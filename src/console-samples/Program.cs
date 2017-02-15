12b18588b1b56284e7cc50fa9917ea3ff0b3a2ea
Andžej Maciusovič <andzej.mac@gmail.com>
2017-02-15 22:07:07 +0200

    mixed cpu and api sample

--- src/console-samples/Program.cs
+++ src/console-samples/Program.cs
@@ -25,7 +25,7 @@ namespace ConsoleApplication
         private static async Task MainAsync()
         {
             Console.WriteLine($"Thread {ThreadId()}");
-            await HttpClientSamples.RunAsync();
+            await MixedSamples.RunAsync();
             Console.WriteLine($"Thread {ThreadId()}");
         }
 
@@ -34,17 +34,6 @@ namespace ConsoleApplication
 
     public class HttpClientSamples
     {
-        public class Request
-        {
-            public string Url { get; }
-            public int Value { get; set; }
-
-            public Request(string url)
-            {
-                Url = url;
-            }
-        }
-
         public static async Task RunAsync()
         {
             var requests = new List<Request>
@@ -54,7 +43,7 @@ namespace ConsoleApplication
                 new Request("http://www.yandex.com")
             };
 
-            var tasks = requests.Select(ProcessApiRequestAsync).ToArray();
+            var tasks = requests.Select(ProcessApiRequestAsync);
             await Task.WhenAll(tasks);
         }
 
@@ -78,7 +67,94 @@ namespace ConsoleApplication
     {
         public static async Task RunAsync()
         {
+            var jobs = new List<Job>
+            {
+                new Job("A", 1),
+                new Job("B", 2)
+            };
 
+            var tasks = jobs.Select(ProcessJobAsync);
+            await Task.WhenAll(tasks);
+        }
+
+        private static Task<bool> ProcessJobAsync(Job job)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"Starting {job.Id} on thread {ThreadId()}");
+                var end = DateTime.Now + TimeSpan.FromSeconds(job.Seconds);
+                while (DateTime.Now < end) ;
+                Console.WriteLine($"Ended {job.Id} on thread {ThreadId()}");
+                return true;
+            });
+        }
+
+        private static Func<int> ThreadId => () => Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public class MixedSamples
+    {
+        public static async Task RunAsync()
+        {
+            var requests = new List<Request>
+            {
+                new Request("http://www.google.com"),
+                new Request("http://www.bing.com"),
+                new Request("http://www.yandex.com")
+            };
+
+            var tasks = requests.Select(ProcessRequestAsync);
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task<HttpResponseMessage> ProcessRequestAsync(Request request)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine($"Starting {request.Url} on thread {ThreadId()}");
+
+                var response = await httpClient.GetAsync(request.Url);
+                var cpuJobResult = await ProcessJobAsync(new Job(request.Url, 2), response);
+
+                Console.WriteLine($"End request {request.Url} on thread {ThreadId()}");
+                return response;
+            }
+        }
+
+        private static Task<bool> ProcessJobAsync(Job job, HttpResponseMessage rsp)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"Starting cpu job {job.Id} on thread {ThreadId()} {rsp.StatusCode}");
+                var end = DateTime.Now + TimeSpan.FromSeconds(job.Seconds);
+                while (DateTime.Now < end) ;
+                Console.WriteLine($"Ended cpu job {job.Id} on thread {ThreadId()}");
+                return true;
+            });
+        }
+
+        private static Func<int> ThreadId => () => Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public class Job
+    {
+        public Job(string id, int time)
+        {
+            Id = id;
+            Seconds = time;
+        }
+        public string Id { get; }
+        public int Seconds { get; }
+    }
+
+    public class Request
+    {
+        public string Url { get; }
+        public int Value { get; set; }
+
+        public Request(string url)
+        {
+            Url = url;
         }
     }
 }
