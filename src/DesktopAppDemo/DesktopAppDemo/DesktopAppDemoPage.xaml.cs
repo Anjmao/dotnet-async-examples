ab9264749df53318314aeaede7c0d846959c8924
Andžej Maciusovič <andzej.mac@gmail.com>
2017-03-12 20:01:49 +0200

    Add desktop ui example

---
+++ src/DesktopAppDemo/DesktopAppDemo/DesktopAppDemoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace DesktopAppDemo
+{
+	public partial class DesktopAppDemoPage : ContentPage
+	{
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			Btn.Text = "Please wait...";
+			DownloadFileAsync().Wait();
+			Btn.Text = "File downloaded";
+		}
+
+		async Task DownloadFileAsync()
+		{
+			await Task.Delay(1000);
+		}
+
+		public DesktopAppDemoPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
